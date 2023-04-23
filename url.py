a72e2a47fceb7ecb7da27401149ae2defaefd981
Martin1134 <44879342+Martin1134@users.noreply.github.com>
2023-04-23 19:46:45 +0000

    new

--- url.py
+++ url.py
@@ -1,36 +1,64 @@
 import requests
+import socket
+import time
 
-# Function to check if a URL is reachable
-def check_url(url):
-    try:
-        response = requests.get(url)
+# Dateiname der Textdatei mit den URLs und Keywords
+url_file = "urls.txt"
+results_file = "results.txt"
+
+# Timeout und Liste der URLs
+timeout = 10
+url_list = []
+
+# URLs und zugehörige Keywords aus der Textdatei lesen
+with open(url_file, "r") as file:
+    for line in file:
+        line = line.strip()
+        if line == "":
+            continue
+        url, keyword = line.split(" ", 1) # die erste Leerzeichen-getrennte Zeichenkette als URL, den Rest als Keyword nehmen
+        url_list.append((url, keyword))
+
+# Ergebnisse in eine Textdatei schreiben
+with open(results_file, "w") as file:
+    # URLs testen
+    for url, keyword in url_list:
+        file.write("Teste URL: " + url + "\n")
+
+        # DNS-Lookup testen
+        try:
+            ip_address = socket.gethostbyname(url)
+            file.write("DNS-Lookup erfolgreich!\n")
+        except socket.gaierror:
+            file.write("DNS-Lookup fehlgeschlagen!\n")
+            continue
+
+        # Verbindungszeit testen
+        start_time = time.time()
+        response = requests.get("http://" + ip_address, timeout=timeout)
+        end_time = time.time()
+        time_taken = end_time - start_time
+
         if response.status_code == 200:
-            return True
+            file.write("Website ist erreichbar!\n")
+            file.write("Verbindungszeit: " + str(time_taken) + " Sekunden\n")
         else:
-            return False
-    except requests.exceptions.RequestException as e:
-        print(e)
-        return False
+            file.write("Website ist nicht erreichbar. Status code: " + str(response.status_code) + "\n")
+            continue
 
-# Function to write results to a text file
-def write_results(results):
-    with open('results.txt', 'w') as file:
-        for result in results:
-            file.write(result + '\n')
+        # Ladegeschwindigkeit testen
+        if time_taken > timeout:
+            file.write("Website-Ladegeschwindigkeit zu langsam!\n")
+        else:
+            file.write("Website-Ladegeschwindigkeit in Ordnung.\n")
 
-# Open the file with the list of URLs and save them to a list
-with open('urls.txt', 'r') as file:
-    urls = [line.strip() for line in file]
+        # Textinhalt testen
+        response_text = response.text.lower()
+        if keyword.lower() in response_text:
+            file.write("Keyword \"" + keyword + "\" gefunden!\n")
+        else:
+            file.write("Keyword nicht gefunden!\n")
 
-# Check each URL in the list and save the results to a list
-results = []
-for url in urls:
-    if check_url(url):
-        result = url + " is reachable"
-    else:
-        result = url + " is not reachable"
-    results.append(result)
+        file.write("\n")  # eine leere Zeile zwischen den Ergebnissen einfügen
 
-# Write the results to a text file
-write_results(results)
-
+print("Testergebnisse wurden in der Datei " + results_file + " gespeichert.")
